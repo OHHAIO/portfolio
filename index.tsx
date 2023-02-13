2e07d9256c61c5f3f2f6bc9e1738768f037a6cc5
Seungjun Oh <ohseungjun1205@gmail.com>
2023-02-13 17:01:21 +0900

    Route 테스트

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import App from "./layouts/App";
-import {createRoot} from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
-
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const container = document.querySelector("#app") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 );
-
